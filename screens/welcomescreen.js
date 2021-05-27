4618fba4b367fa275866d429aa35c1c43deefb4f
vishwapandya <57280185+vishwapandya@users.noreply.github.com>
2021-05-27 17:54:16 +0530

    Update welcomescreen.js

--- screens/welcomescreen.js
+++ screens/welcomescreen.js
@@ -1,127 +1,139 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, Text, TouchableOpacity, TextInput, Alert} from 'react-native';
-import firebase from 'firebase';
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity,TextInput, Alert } from 'react-native';
+
 import db from '../config';
-import SantaAnimation from '../components/SantaClaus';
+import firebase from 'firebase';
 
-export default class WelcomeScreen extends Component{
-    constructor(){
-        super()
-        this.state={
-            emailID: '',
-            password: ''
-        }
+export default class WelcomeScreen extends Component {
+  constructor(){
+    super()
+    this.state={
+      emailID : '',
+      password: ''
     }
+  }
 
-    login=(emailID, password)=>{
-        firebase.auth().signInWithEmailAndPassword(emailID, password)
-        .then(()=>{
-            return alert('Successfully logged in')
-        })
-        .catch((error)=>{
-            var errorMessage = error.message
-            return alert(errorMessage)
-        })
-    }
+  login= (emailID, password)=>{
+    firebase.auth().signInWithEmailAndPassword(emailID, password)
+    .then(()=>{
+      return Alert.alert("Successfully Login")
+    })
+    .catch((error)=> {
+      var errorMessage = error.message;
+      return Alert.alert(errorMessage)
+    })
+  }
 
-    signUp=(emailID, password)=>{
-        firebase.auth().createUserWithEmailAndPassword(emailID, password)
-        .then(()=>{
-            return alert('User added successfully')
-        })
-        .catch((error)=>{
-            var errorMessage = error.message
-            return alert(errorMessage)
-        })
-    }
+    signUp = (emailID, password) =>{
+    firebase.auth().createUserWithEmailAndPassword(emailID, password)
+    .then(()=>{
+      return Alert.alert("User Added Successfully")
+    })
+    .catch(function(error) {
+      var errorMessage = error.message;
+      return Alert.alert(errorMessage)
+    });
+  }
 
-    render(){
-        return(
-            <View style={styles.container}>
-                <View style={styles.profile}>
-                    <Text style={styles.title}> Barter System </Text>
-                </View>
-                <View style={styles.button}>
-                    <TextInput
-                    style={styles.loginBox}
-                    placeholder= 'enter email Id'
-                    placeholderTextColor= 'orange'
-                    keyboardType= 'email-address'
-                    onChangeText={(text)=>{this.setState({emailID: text})}}
-                    />
 
-                    <TextInput
-                    style={styles.loginBox}
-                    placeholder= 'enter password'
-                    placeholderTextColor= 'orange'
-                    secureTextEntry={true}
-                    onChangeText={(text)=>{this.setState({password: text})}}
-                    />
+  render(){
+    return(
+      <View style={styles.container}>
+        <View style={styles.profileContainer}>
+         
+          <Text style={styles.title}>Barter System</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TextInput
+          style={styles.loginBox}
+          placeholder="enter email id "
+          placeholderTextColor = "#ffff"
+          keyboardType ='email-address'
+          onChangeText={(text)=>{
+            this.setState({
+              emailID: text
+            })
+          }}
+        />
 
-                    <TouchableOpacity 
-                    style={[styles.button, {marginBottom: 20, marginTop: 20}]}
-                    onPress={()=>{this.login(this.state.emailID, this.state.password)}}
-                    >
-                    <Text style={styles.buttonText}> Login </Text>    
-                    </TouchableOpacity>
-
-                    <TouchableOpacity 
-                    style={styles.button}
-                    onPress={()=>{this.signUp(this.state.emailID, this.state.password)}}
-                    >
-                    <Text style={styles.buttonText}> Sign Up </Text>    
-                    </TouchableOpacity>
-
-                </View>
-            </View>
-        )
-    }
+        <TextInput
+          style={styles.loginBox}
+          secureTextEntry = {true}
+          placeholder="password"
+          placeholderTextColor = "#ffff"
+          onChangeText={(text)=>{
+            this.setState({
+              password: text
+            })
+          }}
+        />
+          <TouchableOpacity
+            style={[styles.button,{marginBottom:20, marginTop:20}]}
+            onPress = {()=>{this.login(this.state.emailID, this.state.password)}}
+            >
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={()=>{this.signUp(this.state.emailID, this.state.password)}}
+            >
+            <Text style={styles.buttonText}>SignUp</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
 }
 
+
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: '#F8BE85'
+  container:{
+    flex:1,
+    backgroundColor:'#F8BE85'
+  },
+  profileContainer:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  title :{
+    fontSize:65,
+    fontWeight:'300',
+    paddingBottom:30,
+    color : '#ff3d00'
+  },
+  loginBox:{
+    width: 300,
+    height: 40,
+    borderBottomWidth: 1.5,
+    borderColor : '#ff8a65',
+    fontSize: 20,
+    margin:10,
+    paddingLeft:10
+  },
+  button:{
+    width:300,
+    height:50,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:25,
+    backgroundColor:"#ff9800",
+    shadowColor: "#000",
+    shadowOffset: {
+       width: 0,
+       height: 8,
     },
-    profile:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    title:{
-        fontSize: 65,
-        fontWeight: '300',
-        paddingBottom: 30,
-        color: '#FF3D00'
-    },
-    loginBox:{
-        width: 300,
-        height: 40,
-        borderBottomWidth: 1.5,
-        borderColor: '#FF8A65',
-        fontSize: 20,
-        margin: 10,
-        paddingLeft: 10
-    },
-    button:{
-        width: 300,
-        height: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 25,
-        backgroundColor: '#FF9800',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 8
-        },
-        shadowOpacity: 0.30,
-        shadowRadius: 10.30,
-        elevation: 16
-    },
-    buttonText:{
-        color: '#FFF',
-        fontWeight: '200',
-        fontSize: 20
-    }
+    shadowOpacity: 0.30,
+    shadowRadius: 10.32,
+    elevation: 16,
+  },
+  buttonText:{
+    color:'#ffff',
+    fontWeight:'200',
+    fontSize:20
+  },
+  buttonContainer:{
+    flex:1,
+    alignItems:'center'
+  }
 })
